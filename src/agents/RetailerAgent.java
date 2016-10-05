cafc1eb5937b2f9bd569f9afb749b12c3e03f557
fegwin <shwin.lakes@gmail.com>
2016-10-05 18:16:03 +1100

    Various retaileragent upgrades (now takes in params) Removed the random rejecting we were doing

--- src/agents/RetailerAgent.java
+++ src/agents/RetailerAgent.java
@@ -8,11 +8,37 @@ import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
 import jade.proto.ContractNetResponder;
 
+import java.security.InvalidParameterException;
+
 /**
  * Created by fegwin on 7/09/2016.
  */
 public class RetailerAgent extends AbstractAgent {
+    private boolean isOffPeak;
+    private int offPeakTickCount;
+    private int peakTickCount;
+    private int peakPrice;
+    private int offPeakPrice;
+
+    @Override
+    protected void setup() {
+        super.setup();
+
+        Object[] args = getArguments();
+
+        if(args.length < 5) throw new InvalidParameterException("Have not provided starting consumption value");
+
+        isOffPeak = (boolean)args[0];
+        offPeakTickCount = (int)args[1];
+        peakTickCount = (int)args[2];
+        peakPrice = (int)args[3];
+        offPeakPrice = (int)args[4];
+    }
+
     public void configureBehaviours() {
+        // Internal state stuff
+
+        // Negotiation stuff
         MessageTemplate template = MessageTemplate.and(
                 MessageTemplate.MatchProtocol(FIPANames.InteractionProtocol.FIPA_CONTRACT_NET),
                 MessageTemplate.MatchPerformative(ACLMessage.CFP) );
@@ -20,48 +46,31 @@ public class RetailerAgent extends AbstractAgent {
         addBehaviour(new ContractNetResponder(this, template) {
             @Override
             protected ACLMessage handleCfp(ACLMessage cfp) throws NotUnderstoodException, RefuseException {
-                boolean shouldIPropose = testMethodRemoveMe() > 2;
-                
                 /*
                  * NOTE(Lachlan 5-10-16) proposed message format for contract proposals
-                 * "{id=<INT>,sellingAt=<FLOAT>,buyingAt=<FLOAT>,duration=<FLOAT>}{...}{...}"
+                 * "{id=<INT>;sellingAt=<FLOAT>;buyingAt=<FLOAT>;duration=<FLOAT>}{...}{...}"
                  * 
                  */
-                if(shouldIPropose) {
-                    ACLMessage propose = cfp.createReply();
-                    propose.setPerformative(ACLMessage.PROPOSE);
-                    propose.setContent("Yay I'm proposing");
-                    fireStatusChangedEvent("Proposing to " + cfp.getSender().getLocalName());
-                    return propose;
-                } else {
-                    fireStatusChangedEvent("Rejecting cfp from " + cfp.getSender().getLocalName());
-                    throw new RefuseException("We should just be friends. Soz");
-                }
+                ACLMessage propose = cfp.createReply();
+                propose.setPerformative(ACLMessage.PROPOSE);
+                propose.setContent("Yay I'm proposing");
+                fireStatusChangedEvent("Proposing to " + cfp.getSender().getLocalName());
+
+                return propose;
             }
 
             @Override
             protected ACLMessage handleAcceptProposal(ACLMessage cfp, ACLMessage propose,ACLMessage accept) throws FailureException {
-                if (testMethodRemoveMe() > 2) {
                     fireStatusChangedEvent("YAYYYYY :D " + cfp.getSender().getLocalName() + " accepted");
                     ACLMessage inform = accept.createReply();
                     inform.setPerformative(ACLMessage.INFORM);
                     return inform;
-                }
-                else {
-                    fireStatusChangedEvent("wtf happened? " + cfp.getSender().getLocalName());
-                    throw new FailureException("unexpected-error");
-                }
             }
 
             protected void handleRejectProposal(ACLMessage cfp, ACLMessage propose, ACLMessage reject) {
                 fireStatusChangedEvent("Now crying in room :( Eating ice cream because of " + cfp.getSender().getLocalName());
             }
         });
-    }
-
-    private int testMethodRemoveMe() {
-
-        return 10;
     }
 
     @Override
