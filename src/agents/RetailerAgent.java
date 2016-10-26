bc64a40e173e6433f631e6eda8aa576a74f7728e
Lachlan Easton <elbeaston@gmail.com>
2016-10-26 18:14:32 +1100

    Added some documentation for power prices.

--- src/agents/RetailerAgent.java
+++ src/agents/RetailerAgent.java
@@ -21,6 +21,14 @@ import java.util.Vector;
 public class RetailerAgent extends AbstractAgent {
 	private static final long serialVersionUID = 1L;
 
+	
+	/* https://www.originenergy.com.au/terms-and-conditions/qld-electricity-tariffs.html
+	 * Offpeak is on weekends and between 10pm–7am on weekdays
+	 * Shoulder time between 7am–4pm, 8pm–10pm
+	 * 
+	 * Price per kwh averages around ~25-35cents in Australia: https://www.ovoenergy.com/guides/energy-guides/average-electricity-prices-kwh.html
+	 * http://cmeaustralia.com.au/wp-content/uploads/2013/09/FINAL-INTERNATIONAL-PRICE-COMPARISON-FOR-PUBLIC-RELEASE-29-MARCH-2012.pdf
+	 */
 	private boolean isOffPeak;
 
     private int offPeakTickCount;
